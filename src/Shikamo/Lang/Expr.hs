78076833af233430567fea945ded7d276c6fbf08
Providence Salumu <Providence.Salumu@smunix.com>
2018-11-03 18:18:31 -0400

    completes expression parsing + var declaration

--- src/Shikamo/Lang/Expr.hs
+++ src/Shikamo/Lang/Expr.hs
@@ -45,25 +45,40 @@ import qualified Text.Parsec         as P
 import qualified Text.Parsec.Error   as P
 import qualified Text.Parsec.Pos     as P
 
+import Shikamo.Lexer.Loc
+
 -- * Expression AST
 
 data Kind where
   StarKind :: Kind
   FunKind :: Kind -> Kind -> Kind
-  deriving (Typeable, Show, Eq, Ord, Data, Generic)
+  deriving ( Typeable
+           , Show
+           , Eq
+           , Ord
+           , Data
+           , Generic)
 
 data TypeVar i where
   TypeVar :: { tvId :: i
              , tvKind :: !Kind
              } -> TypeVar i
-  deriving (Show, Eq, Generic, Data, Typeable)
+  deriving ( Show
+           , Eq
+           , Generic
+           , Data
+           , Typeable)
 
 -- | A data type constructor.
 data DataCtor t i where
   DataCtor :: { dtcName :: i
               , dtcFields :: [t i]
               } -> DataCtor t i
-  deriving (Show, Eq, Generic, Data, Typeable)
+  deriving ( Show
+           , Eq
+           , Generic
+           , Data
+           , Typeable)
 
 -- | A data type.
 data DataType t i where
@@ -72,80 +87,158 @@ data DataType t i where
               , dtVars :: [TypeVar i]
               , dtCtors :: [DataCtor t i]
               } -> DataType t i
-  deriving (Show, Eq, Generic, Data, Typeable)
+  deriving ( Show
+           , Eq
+           , Generic
+           , Data
+           , Typeable)
 
 -- | A BindGroup represents bindings as in a @LetExpr@
 data BindGroup t i l where
   BindGroup :: { bgExplTypedBindings :: ![ExplTypedBinding t i l]
                , bgImplTypedBindings :: ![ImplTypedBinding t i l]
                } -> BindGroup t i l
-  deriving (Show, Generic, Data, Typeable, Functor, Foldable, Traversable, Eq)
+  deriving ( Show
+           , Generic
+           , Data
+           , Typeable
+           , Functor
+           , Foldable
+           , Traversable
+           , Eq)
 
 -- | A Predicate type that has an @id@ and a @type list@
 data Predicate t i where
   IsIn :: i -> [t i] -> Predicate t i
-  deriving (Show, Eq, Generic, Data, Typeable)
+  deriving ( Show
+           , Eq
+           , Generic
+           , Data
+           , Typeable)
 
 -- | A Qualified type has a list of @Predicate@ to satisfy
 data Qualified t i typ where
   Qualified :: { qualPredicates :: ![Predicate t i]
                , qualType       :: !typ
                } -> Qualified t i typ
-  deriving (Show, Eq, Generic, Data, Typeable)
+  deriving ( Show
+           , Eq
+           , Generic
+           , Data
+           , Typeable)
 
 -- | A type constructor.
 data TypeCtor i where
   TypeCtor :: { typeCtorId   :: !i
               , typeCtorKind :: !Kind
               } -> TypeCtor i
-  deriving (Show, Eq, Generic, Data, Typeable)
+  deriving ( Show
+           , Eq
+           , Generic
+           , Data
+           , Typeable)
 
 -- | A type scheme.
 data Scheme t i typ where
   Forall :: [TypeVar i] -> Qualified t i (typ i) -> Scheme t i typ
-  deriving (Show, Eq, Generic, Data, Typeable)
+  deriving ( Show
+           , Eq
+           , Generic
+           , Data
+           , Typeable)
 
 data CaseAlt t i l where
   CaseAlt :: { caseAltLabel   :: l
              , caseAltPattern :: Pattern t i l
              , caseAltExpr    :: Expr t i l
              } -> CaseAlt t i l
-  deriving (Show, Generic, Data, Typeable, Functor, Foldable, Traversable, Eq)
+  deriving ( Show
+           , Generic
+           , Data
+           , Typeable
+           , Functor
+           , Foldable
+           , Traversable
+           , Eq)
 
 -- | A pattern match
 data Pattern t i l where
-  VarPattern      :: Pattern t i l
-  WildcardPattern :: Pattern t i l
-  AsPattern       :: Pattern t i l
-  LitPattern      :: Pattern t i l
-  CtorPattern     :: Pattern t i l
-  deriving (Show, Generic, Data, Typeable, Functor, Foldable, Traversable, Eq)
+  VarPat      :: l -> i -> Pattern t i l
+  WildcardPat :: l -> i -> Pattern t i l
+  AsPat       :: l -> Pattern t i l
+  LitPat      :: l -> Lit -> Pattern t i l
+  CtorPat     :: l -> i -> [Pattern t i l] -> Pattern t i l
+  deriving ( Show
+           , Generic
+           , Data
+           , Typeable
+           , Functor
+           , Foldable
+           , Traversable
+           , Eq)
+
+instance (l~Loc) => Locate (Pattern t i l) (Pattern t i l) where
+  locate p@(VarPat l _)      = (l, p)
+  locate p@(WildcardPat l _) = (l, p)
+  locate p@(LitPat l _)      = (l, p)
+  locate p@(CtorPat l _ _)   = (l, p)
+  locate p@(AsPat l)         = (l, p)
 
 -- | An Identifier as used in @Shikamo@
-newtype Id = Id { idString :: String }
-  deriving (Show, IsString, Ord, Generic, Data, Typeable, Eq)
+newtype Id = Id { idString :: Text }
+  deriving ( Show
+           , IsString
+           , Ord
+           , Generic
+           , Data
+           , Typeable
+           , Eq)
 
 -- | Type for supported Literals.
 data Lit where
-  IntLit    :: Lit
-  CharLit   :: Lit
-  RatioLit  :: Lit
-  StringLit :: Lit
-  deriving (Show, Generic, Data, Typeable, Eq)
+  IntLit    :: Integer -> Lit
+  CharLit   :: Char -> Lit
+  RatioLit  :: Rational -> Lit
+  StringLit :: Text -> Lit
+  deriving ( Show
+           , Generic
+           , Data
+           , Typeable
+           , Eq)
 
 -- | A Shikamo Expression. (Totally borrowed from Haskell)
 data Expr t i l where
+  UnitExpr :: Expr t i l
   AppExpr    :: l -> Expr t i l -> Expr t i l -> Expr t i l
   CaseExpr   :: l -> Expr t i l -> [CaseAlt t i l] -> Expr t i l
   ConstExpr  :: l -> Id -> Expr t i l
   IfExpr     :: l -> Expr t i l -> Expr t i l -> Expr t i l -> Expr t i l
-  InfixExpr  :: l -> Expr t i l -> (String, Expr t i l) -> Expr t i l -> Expr t i l
+  InfixExpr  :: l -> Expr t i l -> (Text, Expr t i l) -> Expr t i l -> Expr t i l
   LambExpr   :: l -> Alt t i l -> Expr t i l
   LitExpr    :: l -> Lit -> Expr t i l
   LetExpr    :: l -> BindGroup t i l -> Expr t i l -> Expr t i l
   ParensExpr :: l -> Expr t i l -> Expr t i l
   VarExpr    :: l -> i -> Expr t i l
-  deriving (Show, Generic, Data, Typeable, Functor, Foldable, Traversable, Eq)
+  deriving ( Show
+           , Generic
+           , Data
+           , Typeable
+           , Functor
+           , Foldable
+           , Traversable
+           , Eq)
+
+instance (l ~ Loc) => Locate (Expr t i l) (Expr t i l) where
+  locate e@(AppExpr l _ _)     = (l, e)
+  locate e@(CaseExpr l _ _)    = (l, e)
+  locate e@(ConstExpr l _)     = (l, e)
+  locate e@(IfExpr l _ _ _)    = (l, e)
+  locate e@(InfixExpr l _ _ _) = (l, e)
+  locate e@(LambExpr l _)      = (l, e)
+  locate e@(LitExpr l _)       = (l, e)
+  locate e@(LetExpr l _ _)     = (l, e)
+  locate e@(ParensExpr l _)    = (l, e)
+  locate e@(VarExpr l _)       = (l, e)
 
 -- | An Alternative specifies the left and right hand sides of a function definition.
 -- With some complete syntax for Expr, values of type Alternative might be used in the representation of
@@ -155,14 +248,28 @@ data Alt t i l where
          , altPatterns :: ![Pattern t i l]
          , altExprs :: !(Expr t i l)
          } -> Alt t i l
-  deriving (Show, Generic, Data, Typeable, Functor, Foldable, Traversable, Eq)
+  deriving ( Show
+           , Generic
+           , Data
+           , Typeable
+           , Functor
+           , Foldable
+           , Traversable
+           , Eq)
 
 data ImplTypedBinding t i l where
   ImplTypedBinding :: { itbLabel :: l
                       , itbId    :: !(i, l)
                       , itbAlts  :: ![Alt t i l]
                       } -> ImplTypedBinding t i l
-  deriving (Show, Generic, Data, Typeable, Functor, Traversable, Foldable, Eq)
+  deriving ( Show
+           , Generic
+           , Data
+           , Typeable
+           , Functor
+           , Traversable
+           , Foldable
+           , Eq)
 
 -- | Excplicitly typed bindings are described by the name of the fucntion taht is being defined,
 -- the declared type scheme, and the list of alternatives in its definition.
@@ -175,27 +282,52 @@ data ExplTypedBinding t i l where
                       , etbScheme :: !(Scheme t i t)
                       , etbAlts   :: ![Alt t i l]
                       } -> ExplTypedBinding t i l
-  deriving (Show, Generic, Data, Typeable, Functor, Traversable, Foldable, Eq)
+  deriving ( Show
+           , Generic
+           , Data
+           , Typeable
+           , Functor
+           , Traversable
+           , Foldable
+           , Eq)
 
 -- | A binding declaration
 data Binding t i l where
   ImplBinding :: ImplTypedBinding t i l -> Binding t i l
   ExplBinding :: ExplTypedBinding t i l -> Binding t i l
-  deriving (Show, Generic, Data, Typeable)
+  deriving ( Show
+           , Eq
+           , Generic
+           , Data
+           , Typeable)
 
 -- | A Dictionary for a class
 data Dictionary t i l where
   Dictionary :: { dictName :: i
                 , dictMethods :: M.HashMap i (l, Alt t i l)
                 } -> Dictionary t i l
-  deriving (Show, Eq, Generic, Data, Typeable, Functor, Traversable, Foldable)
+  deriving ( Show
+           , Eq
+           , Generic
+           , Data
+           , Typeable
+           , Functor
+           , Traversable
+           , Foldable)
 
 -- | A Class instance.
 data Instance (t :: Type -> Type) i l where
   Instance :: { instPredicate  :: !(Scheme t i (Predicate t))
               , instDictionary :: !(Dictionary t i l)
               } -> Instance t i l
-  deriving (Show, Eq, Generic, Data, Typeable, Functor, Traversable, Foldable)
+  deriving ( Show
+           , Eq
+           , Generic
+           , Data
+           , Typeable
+           , Functor
+           , Traversable
+           , Foldable)
 
 -- | A class.
 data Class (t :: Type -> Type) i l where
@@ -205,12 +337,24 @@ data Class (t :: Type -> Type) i l where
            , className         :: i
            , classMethods      :: M.HashMap i (Scheme t i t)
            } -> Class t i l
-  deriving (Show, Eq, Generic, Data, Typeable, Functor, Traversable, Foldable)
+  deriving ( Show
+           , Eq
+           , Generic
+           , Data
+           , Typeable
+           , Functor
+           , Traversable
+           , Foldable)
 
 -- | A declaration.
 data Decl (t :: Type -> Type) (i :: Type) (l :: Type) where
+  UnitDecl :: Decl t i l
   DataDecl     :: l -> DataType t i -> Decl t i l
   BindDecl     :: l -> Binding t i l -> Decl t i l
   ClassDecl    :: l -> Class t i l -> Decl t i l
   InstanceDecl :: l -> Instance t i l -> Decl t i l
-  deriving (Show, Generic, Data, Typeable)
+  deriving ( Show
+           , Eq
+           , Generic
+           , Data
+           , Typeable)
--- test/Shikamo/Parse/ParserSpec.hs
+++ test/Shikamo/Parse/ParserSpec.hs
@@ -19,6 +19,96 @@ lexemizeT fp s = lexemize fp (s :: Text)
 spec :: Spec
 spec = do
   describe "parser" $ do
+    describe "var/function declariation" $ do
+      it "addition function" $ do
+        lxs <- lexemizeT "<file>" "f :: Int -> Int -> Int\nf = \\x y -> x + y"
+        case lxs of
+          Right lxs' -> do
+            ast <- parseTest varFnDeclExpl "<file>" lxs'
+            ast `shouldBe` Right (BindDecl
+                                   (Loc {locStart = newPos "<file>" 1 1, locEnd = newPos "<file>" 1 2})
+                                   (ExplBinding
+                                     (ExplTypedBinding { etbLabel = Loc {locStart = newPos "<file>" 1 1, locEnd = newPos "<file>" 1 2}
+                                                       , etbId = (Id {idString = "f"},Loc {locStart = newPos "<file>" 1 1, locEnd = newPos "<file>" 1 2})
+                                                       , etbScheme = Forall [] (Qualified { qualPredicates = []
+                                                                                          , qualType = TyUnkindApp
+                                                                                                       (TyUnkindApp (TyUnkindCtor (Id {idString = "(->)"})) (TyUnkindCtor (Id {idString = "Int"})))
+                                                                                                       (TyUnkindApp
+                                                                                                         (TyUnkindApp (TyUnkindCtor (Id {idString = "(->)"})) (TyUnkindCtor (Id {idString = "Int"})))
+                                                                                                         (TyUnkindCtor (Id {idString = "Int"})))
+                                                                                          })
+                                                       , etbAlts = [ Alt { altLabel = Loc {locStart = newPos "<file>" 2 5, locEnd = newPos "<file>" 2 6}
+                                                                         , altPatterns = [ VarPat
+                                                                                           (Loc {locStart = newPos "<file>" 2 6, locEnd = newPos "<file>" 2 7})
+                                                                                           (Id {idString = "x"})
+                                                                                         , VarPat
+                                                                                           (Loc {locStart = newPos "<file>" 2 8, locEnd = newPos "<file>" 2 9})
+                                                                                           (Id {idString = "y"})
+                                                                                         ]
+                                                                         , altExprs = InfixExpr
+                                                                                      (Loc { locStart = newPos "<file>" 2 13, locEnd = newPos "<file>" 2 14})
+                                                                                      (VarExpr
+                                                                                        (Loc {locStart = newPos "<file>" 2 13, locEnd = newPos "<file>" 2 14})
+                                                                                        (Id {idString = "x"}))
+                                                                                      ( "+"
+                                                                                      , VarExpr
+                                                                                        (Loc {locStart = newPos "<file>" 2 15, locEnd = newPos "<file>" 2 16})
+                                                                                        (Id {idString = "+"})
+                                                                                      )
+                                                                                      (VarExpr (Loc {locStart = newPos "<file>" 2 17, locEnd = newPos "<file>" 2 18}) (Id {idString = "y"}))
+                                                                         }
+                                                                   ]
+                                                       })))
+
+      it "function" $ do
+        lxs <- lexemizeT "<file>" "f :: Int -> Int\nf = \\x -> x"
+        case lxs of
+          Right lxs' -> do
+            ast <- parseTest varFnDeclExpl "<file>" lxs'
+            ast `shouldBe` Right (BindDecl
+                                   (Loc {locStart = newPos "<file>" 1 1, locEnd = newPos "<file>" 1 2})
+                                   (ExplBinding (ExplTypedBinding { etbLabel = Loc { locStart = newPos "<file>" 1 1, locEnd = newPos "<file>" 1 2 }
+                                                                  , etbId = (Id {idString = "f"},Loc {locStart = newPos "<file>" 1 1, locEnd = newPos "<file>" 1 2})
+                                                                  , etbScheme = Forall [] (Qualified { qualPredicates = []
+                                                                                                     , qualType = TyUnkindApp
+                                                                                                                  (TyUnkindApp
+                                                                                                                    (TyUnkindCtor (Id {idString = "(->)"}))
+                                                                                                                    (TyUnkindCtor (Id {idString = "Int"})))
+                                                                                                                  (TyUnkindCtor (Id {idString = "Int"}))})
+                                                                  , etbAlts = [ Alt { altLabel = Loc {locStart = newPos "<file>" 2 5, locEnd = newPos "<file>" 2 6}
+                                                                                    , altPatterns = [ VarPat
+                                                                                                      (Loc {locStart = newPos "<file>" 2 6, locEnd = newPos "<file>" 2 7})
+                                                                                                      (Id {idString = "x"})]
+                                                                                    , altExprs = VarExpr
+                                                                                                 (Loc {locStart = newPos "<file>" 2 11, locEnd = newPos "<file>" 2 12})
+                                                                                                 (Id {idString = "x"})}
+                                                                              ]
+                                                                  })))
+
+    describe "expr declaration" $ do
+      it "lambda" $ do
+        lxs <- lexemizeT "<file>" "\\x -> x"
+        case lxs of
+          Right lxs' -> do
+            ast <- parseTest expr "<file>" lxs'
+            ast `shouldBe` Right (LambExpr
+                                   (Loc { locStart = newPos "<file>" 1 1
+                                        , locEnd = newPos "<file>" 1 2})
+                                   (Alt { altLabel = Loc { locStart = newPos "<file>" 1 1
+                                                         , locEnd = newPos "<file>" 1 2
+                                                         }
+                                        , altPatterns = [ VarPat
+                                                          (Loc { locStart = newPos "<file>" 1 2
+                                                               , locEnd = newPos "<file>" 1 3
+                                                               })
+                                                          (Id {idString = "x"})
+                                                        ]
+                                        , altExprs = VarExpr
+                                                     (Loc { locStart = newPos "<file>" 1 7
+                                                          , locEnd = newPos "<file>" 1 8
+                                                          })
+                                                     (Id {idString = "x"})}))
+
     describe "data declaration" $ do
       it "dataDecl => data Maybe (a :: Type) = Just a | Nothing" $ do
         lxs <- lexemizeT "<file>" "data Maybe (a :: Type) = Just a | Nothing"
